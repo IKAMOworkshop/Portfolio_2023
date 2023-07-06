0d627cf818c83bba106c7d84b0c7f0c5b19bb634
Jeremy Chang <changjeremy0226@gmail.com>
2023-07-06 09:03:48 -0700

    Finialize home - start on project page layout.

--- Interactions/menu.js
+++ Interactions/menu.js
@@ -10,11 +10,45 @@ function toggleMenu() {
 };
 
 function openMenu() {
-    navMenu.style.opacity = "1";
-};
+    navMenu.style.display = "flex";
+    setTimeout(function(){
+        navMenu.style.opacity = "1";
+    }, 1)
+}
 
 function closeMenu() {
     navMenu.style.opacity = "0";
+    setTimeout(function(){
+        navMenu.style.display = "none";
+    }, 1000)
 };
 
 navButton.addEventListener('click', toggleMenu);
+
+const scrollProgressBar = document.querySelector("#progress-bar");
+
+function scrollProgress() {
+    const totalHeight = document.body.scrollHeight;
+    const currentDistance = document.documentElement.scrollTop;
+    const windowHeight = document.documentElement.clientHeight;
+
+    const scrolledPercentage = (currentDistance / (totalHeight - windowHeight)) * 100;
+
+    scrollProgressBar.style.height = Math.round(scrolledPercentage) + "%";
+}
+
+document.addEventListener('scroll', scrollProgress);
+
+const emailButton = document.getElementsByClassName('email-link');
+
+function copyEmail() {
+    navigator.clipboard.writeText("changjeremy0226@gmail.com").then(() => {
+        console.log('Content copied to clipboard');
+        /* Resolved - text copied to clipboard successfully */
+      },() => {
+        console.error('Failed to copy');
+        /* Rejected - text failed to copy to the clipboard */
+      });
+}
+
+emailButton.addEventListener('click', copyEmail);
---
+++ Interactions/smooth.js
@@ -0,0 +1,8 @@
+var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+if (!isMobile) {
+     luxy.init({
+       wrapper: '#luxy',
+       wrapperSpeed: 0.065,
+      
+   });
+}

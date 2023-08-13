990f85b1e9f12dc2a1d92337e5f87a2f01efb4b8
Jeremy Chang <changjeremy0226@gmail.com>
2023-08-12 21:28:59 -0700

    Added cursor hover animation

--- Interactions/cursor.js
+++ Interactions/cursor.js
@@ -1,5 +1,7 @@
 const cursor = document.getElementById("cursor");
 const gradient = document.getElementById("gradient");
+const allButton = document.getElementsByTagName("button");
+const allHyperlink = document.getElementsByTagName("a");
 
 document.body.onpointermove = event => {
   const { clientX, clientY } = event;
@@ -16,3 +18,27 @@ document.body.onpointermove = event => {
   }, {duration: 10000, fill: "forwards"
   });
 };
+
+for (var i = 0 ; i < allButton.length ; i++){
+  allButton[i].addEventListener("mouseover", function(){
+    cursor.style.transform = "scale(1.5)";
+  });
+}
+
+for (var i = 0 ; i < allButton.length ; i++){
+  allButton[i].addEventListener("mouseout", function(){
+    cursor.style.transform = "scale(1)";
+  });
+}
+
+for (var i = 0 ; i < allHyperlink.length ; i++){
+  allHyperlink[i].addEventListener("mouseover", function(){
+    cursor.style.transform = "scale(1.5)";
+  })
+}
+
+for (var i = 1 ; i < allHyperlink.length ; i++){
+  allHyperlink[i].addEventListener("mouseout", function(){
+    cursor.style.transform = "scale(1)";
+  })
+}

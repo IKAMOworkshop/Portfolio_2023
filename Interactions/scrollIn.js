f3742febdada0d73c4e51af9dd832b6404dc76e9
Jeremy Chang <changjeremy0226@gmail.com>
2023-08-14 09:44:55 -0700

    Update timing for landing animation

--- Interactions/scrollIn.js
+++ Interactions/scrollIn.js
@@ -3,7 +3,7 @@ const landingPage = document.getElementById("top-page");
 function landingAnimation(){
     setTimeout(function(){
         landingPage.style.transform = "scale(1)";
-    }, 1200);
+    }, 300);
 }
 
 if(landingPage){
